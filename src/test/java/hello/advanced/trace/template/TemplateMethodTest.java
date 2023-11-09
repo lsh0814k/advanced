07777aab218aa68eede71d79cea6d82738d91310
lsg0814k <aa134759>
2023-11-09 21:56:11 +0900

    기본적인 템플릿 메서드 익명 내부 클래스로 구현

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -43,7 +43,27 @@ public class TemplateMethodTest {
         template1.execute();
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
+    }
+
+    @Test
+    void temploateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1); // 클래스 이름1 = TemplateMethodTest$1
+        template1.execute();
 
 
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2 = {}", template2); // 클래스 이름2 = TemplateMethodTest$2
+        template2.execute();
     }
 }
