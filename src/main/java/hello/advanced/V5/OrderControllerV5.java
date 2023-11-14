cfe9abd5e931b949a7c19682bc553aa76f6797bd
이상훈 <slee@i-magicsoft.com>
2023-11-14 14:34:18 +0900

    템플릿 콜백 패턴 적용

---
+++ src/main/java/hello/advanced/V5/OrderControllerV5.java
@@ -0,0 +1,27 @@
+package hello.advanced.V5;
+
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate traceTemplate;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace logTrace) {
+        this.orderService = orderService;
+        this.traceTemplate = new TraceTemplate(logTrace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return traceTemplate.execute("OrderController.request()", () -> {
+            orderService.orderItem(itemId);
+            return "ok";
+        });
+    }
+}
