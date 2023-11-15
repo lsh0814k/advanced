a331a7e57191d885708c57cc424c287ea2cfadfc
이상훈 <slee@i-magicsoft.com>
2023-11-15 10:44:53 +0900

    데코레이터 패턴 적용 테스트 코드

--- src/test/java/hello/advanced/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/advanced/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.advanced.pureproxy.decorator;
 
 import hello.advanced.pureproxy.decorator.code.Component;
 import hello.advanced.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.advanced.pureproxy.decorator.code.MessageDecorator;
 import hello.advanced.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component component = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(component);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(component);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
