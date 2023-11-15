1f56297f4763b287e0b73f038be953e50d1bcaba
이상훈 <slee@i-magicsoft.com>
2023-11-15 15:52:06 +0900

    구체 클래스 기반 프록시 적용 테스트 코드

--- src/test/java/hello/advanced/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/advanced/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.advanced.pureproxy.concreteproxy;
 
 import hello.advanced.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.advanced.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.advanced.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -9,6 +10,12 @@ public class ConcreteProxyTest {
     @Test
     void noProxy() {
         ConcreteClient client = new ConcreteClient(new ConcreteLogic());
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteClient client = new ConcreteClient(new TimeProxy(new ConcreteLogic()));
         client.execute();
     }
 }
---
+++ src/test/java/hello/advanced/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,24 @@
+package hello.advanced.pureproxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeProxy extends ConcreteLogic {
+    private ConcreteLogic target;
+
+    public TimeProxy(ConcreteLogic target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+        String result = target.operation();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}ms", resultTime);
+
+        return result;
+    }
+}
