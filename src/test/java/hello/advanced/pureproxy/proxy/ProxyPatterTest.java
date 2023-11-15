94a72ea59ce47ccda91033362bbc09a4741e4677
이상훈 <slee@i-magicsoft.com>
2023-11-15 10:10:11 +0900

    프록시 적용 테스트 코드

--- src/test/java/hello/advanced/pureproxy/proxy/ProxyPatterTest.java
+++ src/test/java/hello/advanced/pureproxy/proxy/ProxyPatterTest.java
@@ -1,5 +1,6 @@
 package hello.advanced.pureproxy.proxy;
 
+import hello.advanced.pureproxy.proxy.code.CacheProxy;
 import hello.advanced.pureproxy.proxy.code.ProxyPatternClient;
 import hello.advanced.pureproxy.proxy.code.RealSubject;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,14 @@ public class ProxyPatterTest {
     @Test
     void noProxyTest() {
         ProxyPatternClient client = new ProxyPatternClient(new RealSubject());
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyText() {
+        ProxyPatternClient client = new ProxyPatternClient(new CacheProxy(new RealSubject()));
         client.execute();
         client.execute();
         client.execute();
