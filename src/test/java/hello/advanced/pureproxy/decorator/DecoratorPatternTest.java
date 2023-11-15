ad4356c39edb696f39306c8d7a5fadf53fb63cd5
이상훈 <slee@i-magicsoft.com>
2023-11-15 11:19:27 +0900

    데코레이터 패턴 체인 적용 테스트 코드

--- src/test/java/hello/advanced/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/advanced/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.advanced.pureproxy.decorator;
 
-import hello.advanced.pureproxy.decorator.code.Component;
-import hello.advanced.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.advanced.pureproxy.decorator.code.MessageDecorator;
-import hello.advanced.pureproxy.decorator.code.RealComponent;
+import hello.advanced.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +19,15 @@ public class DecoratorPatternTest {
         Component component = new RealComponent();
         MessageDecorator messageDecorator = new MessageDecorator(component);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component component = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(component);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
