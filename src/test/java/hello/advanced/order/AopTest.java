0cf1ad45790a7862cee1ab1aec4bcd1f6f91442e
이상훈 <slee@i-magicsoft.com>
2023-11-24 15:10:48 +0900

    어드바이스 순서 적용

--- src/test/java/hello/advanced/order/AopTest.java
+++ src/test/java/hello/advanced/order/AopTest.java
@@ -1,9 +1,6 @@
 package hello.advanced.order;
 
-import hello.advanced.order.aop.AspectV1;
-import hello.advanced.order.aop.AspectV2;
-import hello.advanced.order.aop.AspectV3;
-import hello.advanced.order.aop.AspectV4Pointcut;
+import hello.advanced.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +14,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired OrderService orderService;
